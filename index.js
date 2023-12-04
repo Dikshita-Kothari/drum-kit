94e179c155175e37d973dec37ab16e12ec430754
Dikshita-Kothari <dikshitakothari11@gmail.com>
2023-12-04 17:59:50 +0530

    complete drum kit

---
+++ index.js
@@ -0,0 +1,73 @@
+// selecting all buttons
+let btn = document.querySelectorAll(".drum");
+
+
+// add event listener to it (click)
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function(){
+
+        let WhichButtonIsClicked=this.innerHTML;
+        makesound(WhichButtonIsClicked);
+        btnAnnimation(WhichButtonIsClicked);
+
+    });
+}
+
+
+// add event to whole page (keypress)
+document.addEventListener("keypress", function (event) {
+
+    makesound(event.key);
+    btnAnnimation(event.key);
+
+});
+
+
+// function for sounds
+function makesound(el) {
+    switch (el) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        default:
+            alert("not valid");
+            break;
+    }
+}
+
+
+// annimation in button
+function btnAnnimation(el){
+
+    let activebtn= document.querySelector("."+el);
+    activebtn.classList.add("pressed");
+
+    setTimeout(function(){
+        activebtn.classList.remove("pressed");
+    },100);
+}
